c96e071272e2f6fde5bdcc97d8ed89fa2dc24078
hehefan <crane.h.fan@gmail.com>
2021-04-06 15:20:13 +0800

    modules

---
+++ modules-pytorch-1.8.1/point_4d_convolution.py
@@ -0,0 +1,271 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+import math
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+
+import pointnet2_utils
+from typing import List
+
+
+class P4DConv(nn.Module):
+    def __init__(self,
+                 in_planes: int,
+                 mlp_planes: List[int],
+                 mlp_batch_norm: List[bool],
+                 mlp_activation: List[bool],
+                 spatial_kernel_size: [float, int],
+                 spatial_stride: int,
+                 temporal_kernel_size: int,
+                 temporal_stride: int = 1,
+                 temporal_padding: [int, int] = [0, 0],
+                 temporal_padding_mode: str = 'replicate',
+                 operator: str = 'addition',
+                 spatial_pooling: str = 'max',
+                 temporal_pooling: str = 'sum',
+                 bias: bool = False):
+
+        super().__init__()
+
+        self.in_planes = in_planes
+        self.mlp_planes = mlp_planes
+        self.mlp_batch_norm = mlp_batch_norm
+        self.mlp_activation = mlp_activation
+
+        self.r, self.k = spatial_kernel_size
+        self.spatial_stride = spatial_stride
+
+        self.temporal_kernel_size = temporal_kernel_size
+        self.temporal_stride = temporal_stride
+        self.temporal_padding = temporal_padding
+        self.temporal_padding_mode = temporal_padding_mode
+
+        self.operator = operator
+        self.spatial_pooling = spatial_pooling
+        self.temporal_pooling = temporal_pooling
+
+        conv_d = [nn.Conv2d(in_channels=4, out_channels=mlp_planes[0], kernel_size=1, stride=1, padding=0, bias=bias)]
+        if mlp_batch_norm[0]:
+            conv_d.append(nn.BatchNorm2d(num_features=mlp_planes[0]))
+        if mlp_activation[0]:
+            conv_d.append(nn.ReLU(inplace=True))
+        self.conv_d = nn.Sequential(*conv_d)
+
+        if in_planes != 0:
+            conv_f = [nn.Conv2d(in_channels=in_planes, out_channels=mlp_planes[0], kernel_size=1, stride=1, padding=0, bias=bias)]
+            if mlp_batch_norm[0]:
+                conv_f.append(nn.BatchNorm2d(num_features=mlp_planes[0]))
+            if mlp_activation[0]:
+                conv_f.append(nn.ReLU(inplace=True))
+            self.conv_f = nn.Sequential(*conv_f)
+
+        mlp = []
+        for i in range(1, len(mlp_planes)):
+            if mlp_planes[i] != 0:
+                mlp.append(nn.Conv2d(in_channels=mlp_planes[i-1], out_channels=mlp_planes[i], kernel_size=1, stride=1, padding=0, bias=bias))
+            if mlp_batch_norm[i]:
+                mlp.append(nn.BatchNorm2d(num_features=mlp_planes[i]))
+            if mlp_activation[i]:
+                mlp.append(nn.ReLU(inplace=True))
+        self.mlp = nn.Sequential(*mlp)
+
+
+    def forward(self, xyzs: torch.Tensor, features: torch.Tensor = None) -> (torch.Tensor, torch.Tensor):
+        """
+        Args:
+            xyzs: torch.Tensor
+                 (B, T, N, 3) tensor of sequence of the xyz coordinates
+            features: torch.Tensor
+                 (B, T, C, N) tensor of sequence of the features
+        """
+        device = xyzs.get_device()
+
+        nframes = xyzs.size(1)
+        npoints = xyzs.size(2)
+
+        assert (self.temporal_kernel_size % 2 == 1), "P4DConv: Temporal kernel size should be odd!"
+        assert ((nframes + sum(self.temporal_padding) - self.temporal_kernel_size) % self.temporal_stride == 0), "P4DConv: Temporal length error!"
+
+        xyzs = torch.split(tensor=xyzs, split_size_or_sections=1, dim=1)
+        xyzs = [torch.squeeze(input=xyz, dim=1).contiguous() for xyz in xyzs]
+
+        if self.temporal_padding_mode == 'zeros':
+            xyz_padding = torch.zeros(xyzs[0].size(), dtype=torch.float32, device=device)
+            for i in range(self.temporal_padding[0]):
+                xyzs = [xyz_padding] + xyzs
+            for i in range(self.temporal_padding[1]):
+                xyzs = xyzs + [xyz_padding]
+        else:
+            for i in range(self.temporal_padding[0]):
+                xyzs = [xyzs[0]] + xyzs
+            for i in range(self.temporal_padding[1]):
+                xyzs = xyzs + [xyzs[-1]]
+
+        if self.in_planes != 0:
+            features = torch.split(tensor=features, split_size_or_sections=1, dim=1)
+            features = [torch.squeeze(input=feature, dim=1).contiguous() for feature in features]
+
+            if self.temporal_padding_mode == 'zeros':
+                feature_padding = torch.zeros(features[0].size(), dtype=torch.float32, device=device)
+                for i in range(self.temporal_padding[0]):
+                    features = [feature_padding] + features
+                for i in range(self.temporal_padding[1]):
+                    features = features + [feature_padding]
+            else:
+                for i in range(self.temporal_padding[0]):
+                    features = [features[0]] + features
+                for i in range(self.temporal_padding[1]):
+                    features = features + [features[-1]]
+
+        new_xyzs = []
+        new_features = []
+        for t in range(self.temporal_kernel_size//2, len(xyzs)-self.temporal_kernel_size//2, self.temporal_stride):                 # temporal anchor frames
+            # spatial anchor point subsampling by FPS
+            anchor_idx = pointnet2_utils.furthest_point_sample(xyzs[t], npoints//self.spatial_stride)                               # (B, N//self.spatial_stride)
+            anchor_xyz_flipped = pointnet2_utils.gather_operation(xyzs[t].transpose(1, 2).contiguous(), anchor_idx)                 # (B, 3, N//self.spatial_stride)
+            anchor_xyz_expanded = torch.unsqueeze(anchor_xyz_flipped, 3)                                                            # (B, 3, N//spatial_stride, 1)
+            anchor_xyz = anchor_xyz_flipped.transpose(1, 2).contiguous()                                                            # (B, N//spatial_stride, 3)
+
+            new_feature = []
+            for i in range(t-self.temporal_kernel_size//2, t+self.temporal_kernel_size//2+1):
+                neighbor_xyz = xyzs[i]
+
+                idx = pointnet2_utils.ball_query(self.r, self.k, neighbor_xyz, anchor_xyz)
+
+                neighbor_xyz_flipped = neighbor_xyz.transpose(1, 2).contiguous()                                                    # (B, 3, N)
+                neighbor_xyz_grouped = pointnet2_utils.grouping_operation(neighbor_xyz_flipped, idx)                                # (B, 3, N//spatial_stride, k)
+
+                xyz_displacement = neighbor_xyz_grouped - anchor_xyz_expanded                                                       # (B, 3, N//spatial_stride, k)
+                t_displacement = torch.ones((xyz_displacement.size()[0], 1, xyz_displacement.size()[2], xyz_displacement.size()[3]), dtype=torch.float32, device=device) * (i-t)
+                displacement = torch.cat(tensors=(xyz_displacement, t_displacement), dim=1, out=None)                               # (B, 4, N//spatial_stride, k)
+                displacement = self.conv_d(displacement)
+
+                if self.in_planes != 0:
+                    neighbor_feature_grouped = pointnet2_utils.grouping_operation(features[i], idx)                                 # (B, in_planes, N//spatial_stride, k)
+                    feature = self.conv_f(neighbor_feature_grouped)
+                    if self.operator == '+':
+                        feature = feature + displacement
+                    else:
+                        feature = feature * displacement
+                else:
+                    feature = displacement
+
+                feature = self.mlp(feature)
+                if self.spatial_pooling == 'max':
+                    feature = torch.max(input=feature, dim=-1, keepdim=False)[0]                                                        # (B, out_planes, n)
+                elif self.spatial_pooling == 'sum':
+                    feature = torch.sum(input=feature, dim=-1, keepdim=False)
+                else:
+                    feature = torch.mean(input=feature, dim=-1, keepdim=False)
+
+                new_feature.append(feature)
+            new_feature = torch.stack(tensors=new_feature, dim=1)
+            if self.temporal_pooling == 'max':
+                new_feature = torch.max(input=new_feature, dim=1, keepdim=False)[0]
+            elif self.temporal_pooling == 'sum':
+                new_feature = torch.sum(input=new_feature, dim=1, keepdim=False)
+            else:
+                new_feature = torch.mean(input=new_feature, dim=1, keepdim=False)
+            new_xyzs.append(anchor_xyz)
+            new_features.append(new_feature)
+
+        new_xyzs = torch.stack(tensors=new_xyzs, dim=1)
+        new_features = torch.stack(tensors=new_features, dim=1)
+
+        return new_xyzs, new_features
+
+class P4DTransConv(nn.Module):
+    def __init__(self,
+                 in_planes: int,
+                 mlp_planes: List[int],
+                 mlp_batch_norm: List[bool],
+                 mlp_activation: List[bool],
+                 original_planes: int = 0,
+                 bias: bool = False):
+        """
+        Args:
+            in_planes: C'. when point features are not available, in_planes is 0.
+            out_planes: C"
+            original_planes: skip connection from original points. when original point features are not available, original_in_planes is 0.
+            bias: whether to use bias
+            batch_norm: whether to use batch norm
+            activation:
+        """
+        super().__init__()
+
+        self.in_planes = in_planes
+        self.mlp_planes = mlp_planes
+        self.mlp_batch_norm = mlp_batch_norm
+
+        conv = []
+        for i in range(len(mlp_planes)):
+            if i == 0:
+                conv.append(nn.Conv1d(in_channels=in_planes+original_planes, out_channels=mlp_planes[i], kernel_size=1, stride=1, padding=0, bias=bias))
+            else:
+                conv.append(nn.Conv1d(in_channels=mlp_planes[i-1], out_channels=mlp_planes[i], kernel_size=1, stride=1, padding=0, bias=bias))
+            if mlp_batch_norm[i]:
+                conv.append(nn.BatchNorm1d(num_features=mlp_planes[i]))
+            if mlp_activation[i]:
+                conv.append(nn.ReLU(inplace=True))
+        self.conv = nn.Sequential(*conv)
+
+    def forward(self, xyzs: torch.Tensor, original_xyzs: torch.Tensor, features: torch.Tensor, original_features: torch.Tensor = None) -> torch.Tensor:
+        r"""
+        Parameters
+        ----------
+        xyzs : torch.Tensor
+            (B, T, N', 3) tensor of the xyz positions of the convolved features
+        original_xyzs : torch.Tensor
+            (B, T, N, 3) tensor of the xyz positions of the original points
+        features : torch.Tensor
+            (B, T, C', N') tensor of the features to be propigated to
+        original_features : torch.Tensor
+            (B, T, C, N) tensor of original point features for skip connection
+
+        Returns
+        -------
+        new_features : torch.Tensor
+            (B, T, C", N) tensor of the features of the unknown features
+        """
+
+        T = xyzs.size(1)
+
+        xyzs = torch.split(tensor=xyzs, split_size_or_sections=1, dim=1)
+        xyzs = [torch.squeeze(input=xyz, dim=1).contiguous() for xyz in xyzs]
+
+        features = torch.split(tensor=features, split_size_or_sections=1, dim=1)
+        features = [torch.squeeze(input=feature, dim=1).contiguous() for feature in features]
+
+        new_xyzs = original_xyzs
+
+        original_xyzs = torch.split(tensor=original_xyzs, split_size_or_sections=1, dim=1)
+        original_xyzs = [torch.squeeze(input=original_xyz, dim=1).contiguous() for original_xyz in original_xyzs]
+
+        if original_features is not None:
+            original_features = torch.split(tensor=original_features, split_size_or_sections=1, dim=1)
+            original_features = [torch.squeeze(input=feature, dim=1).contiguous() for feature in original_features]
+
+        new_features = []
+
+        for t in range(T):
+            dist, idx = pointnet2_utils.three_nn(original_xyzs[t], xyzs[t])
+
+            dist_recip = 1.0 / (dist + 1e-8)
+            norm = torch.sum(dist_recip, dim=2, keepdim=True)
+            weight = dist_recip / norm
+
+            interpolated_feat = pointnet2_utils.three_interpolate(features[t], idx, weight)
+
+            if original_features is not None:
+                new_feature = torch.cat([interpolated_feat, original_features[t]], dim=1)
+            new_feature = self.conv(new_feature)
+            new_features.append(new_feature)
+
+        new_features = torch.stack(tensors=new_features, dim=1)
+
+        return new_xyzs, new_features
---
+++ modules-pytorch-1.8.1/setup.py
@@ -0,0 +1,34 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import glob
+import os
+
+_ext_src_root = "_ext_src"
+_ext_sources = glob.glob("{}/src/*.cpp".format(_ext_src_root)) + glob.glob(
+    "{}/src/*.cu".format(_ext_src_root)
+)
+_ext_headers = glob.glob("{}/include/*".format(_ext_src_root))
+
+headers = "-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), '_ext_src', 'include')
+
+setup(
+    name='pointnet2',
+    ext_modules=[
+        CUDAExtension(
+            name='pointnet2._ext',
+            sources=_ext_sources,
+            extra_compile_args={
+                "cxx": ["-O2", headers],
+                "nvcc": ["-O2", headers],
+            },
+        )
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    }
+)
