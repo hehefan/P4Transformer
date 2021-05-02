0d813eba3f5b38a1bd26466eb1804bfd3ba484f2
hehefan <crane.h.fan@gmail.com>
2021-05-02 17:13:49 +0800

    MSR-Action3D

---
+++ datasets/msr.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import numpy as np
+from torch.utils.data import Dataset
+
+class MSRAction3D(Dataset):
+    def __init__(self, root, frames_per_clip=16, step_between_clips=1, num_points=2048, train=True):
+        super(MSRAction3D, self).__init__()
+
+        self.videos = []
+        self.labels = []
+        self.index_map = []
+        index = 0
+        for video_name in os.listdir(root):
+            if train and (int(video_name.split('_')[1].split('s')[1]) <= 5):
+                video = np.load(os.path.join(root, video_name), allow_pickle=True)['point_clouds']
+                self.videos.append(video)
+                label = int(video_name.split('_')[0][1:])-1
+                self.labels.append(label)
+
+                nframes = video.shape[0]
+                for t in range(0, nframes-step_between_clips*(frames_per_clip-1), step_between_clips):
+                    self.index_map.append((index, t))
+                index += 1
+
+            if not train and (int(video_name.split('_')[1].split('s')[1]) > 5):
+                video = np.load(os.path.join(root, video_name), allow_pickle=True)['point_clouds']
+                self.videos.append(video)
+                label = int(video_name.split('_')[0][1:])-1
+                self.labels.append(label)
+
+                nframes = video.shape[0]
+                for t in range(0, nframes-step_between_clips*(frames_per_clip-1), step_between_clips):
+                    self.index_map.append((index, t))
+                index += 1
+
+        self.frames_per_clip = frames_per_clip
+        self.step_between_clips = step_between_clips
+        self.num_points = num_points
+        self.train = train
+        self.num_classes = max(self.labels) + 1
+
+
+    def __len__(self):
+        return len(self.index_map)
+
+    def __getitem__(self, idx):
+        index, t = self.index_map[idx]
+
+        video = self.videos[index]
+        label = self.labels[index]
+
+        clip = [video[t+i*self.step_between_clips] for i in range(self.frames_per_clip)]
+        for i, p in enumerate(clip):
+            if p.shape[0] > self.num_points:
+                r = np.random.choice(p.shape[0], size=self.num_points, replace=False)
+            else:
+                repeat, residue = self.num_points // p.shape[0], self.num_points % p.shape[0]
+                r = np.random.choice(p.shape[0], size=residue, replace=False)
+                r = np.concatenate([np.arange(p.shape[0]) for _ in range(repeat)] + [r], axis=0)
+            clip[i] = p[r, :]
+        clip = np.array(clip)
+
+        if self.train:
+            # scale the points
+            scales = np.random.uniform(0.9, 1.1, size=3)
+            clip = clip * scales
+
+        clip = clip / 300
+
+        return clip.astype(np.float32), label, index
+
+if __name__ == '__main__':
+    dataset = MSRAction(root='../data/msr_action', frames_per_clip=16)
+    clip, label, video_idx = dataset[0]
+    print(clip)
+    print(label)
+    print(video_idx)
+    print(dataset.num_classes)
